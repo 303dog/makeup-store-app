539c7ab7ee25054aeca250268847750bb6bac5ee
jessmitch42 <jessicamitch42@gmail.com>
2018-01-24 16:58:12 -0500

    css added and started on main page

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,5 +1,11 @@
 class ProductsController < ApplicationController
 
+  def slug(id)
+    p = Product.find(id)
+    brand = p.brand.gsub(" ", "-")
+    name = p.name.gsub(" ", "-")
+    "#{brand}-#{name}"
+  end
   # **** ALL PRODUCTS / LANDING PAGE **** Viewable without account
   get '/products' do
     @products = Product.all
