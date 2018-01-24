706ae2096856f06579cb0a7fdd3be660dcf292a5
jessmitch42 <jessicamitch42@gmail.com>
2018-01-24 18:45:25 -0500

    style updates

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -2,6 +2,7 @@ class ProductsController < ApplicationController
 
   # **** ALL PRODUCTS / LANDING PAGE **** Viewable without account
   get '/products' do
+    @name = "All Products"
     @products = Product.all
     erb :'products/show_all'
   end
@@ -9,16 +10,19 @@ class ProductsController < ApplicationController
   # ***** SORT *****
 
   get '/products/lipstick' do
+    @name = "All Lipsticks"
     @products = Product.where(category: "lipstick")
     erb :'products/show_all'
   end
 
   get '/products/eyeshadow' do
+    @name = "All Eyeshadows"
     @products = Product.where(category: "eyeshadow")
     erb :'products/show_all'
   end
 
   get '/products/blush' do
+    @name = "All Blushes"
     @products = Product.where(category: "blush")
     erb :'products/show_all'
   end
