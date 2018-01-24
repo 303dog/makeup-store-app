1cea800e71fa488cc3b2d6b4668793a590a77708
jessmitch42 <jessicamitch42@gmail.com>
2018-01-24 17:03:16 -0500

    product slugable

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,15 +1,10 @@
 class ProductsController < ApplicationController
-
-  def slug(id)
-    p = Product.find(id)
-    brand = p.brand.gsub(" ", "-")
-    name = p.name.gsub(" ", "-")
-    "#{brand}-#{name}"
-  end
   # **** ALL PRODUCTS / LANDING PAGE **** Viewable without account
   get '/products' do
     @products = Product.all
     erb :'products/show_all'
   end
+
+  get '/products/:slug'
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ActiveRecord::Base
   # products belongs to a cart
   belongs_to :cart
+
+  def slug
+    "#{self.name.gsub(' ', '-').downcase}-#{self.name.gsub(' ', '-').downcase}"
+  end
+
+  def self.find_by_slug(slug)
+    Product.all.find{|i| i.slug == slug}
+  end
 end
